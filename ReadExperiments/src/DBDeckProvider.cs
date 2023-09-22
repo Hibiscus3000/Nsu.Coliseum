220ae418c08258cd10406338481a0c36bdb246de
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2023-09-22 16:43:18 +0700

    task4: added pagination read from db

--- ReadExperiments/src/DBDeckProvider.cs
+++ ReadExperiments/src/DBDeckProvider.cs
@@ -3,56 +3,75 @@ using StrategyInterface;
 
 namespace ReadExperiments;
 
+//TODO add number of experiments limit
 public class DBDeckProvider : IDeckProvider
 {
-    //private static readonly int ReadAtOnce = 1000;
-    //private int currentPage;
+    private const int ReadAtOnce = 20;
 
-    private List<ExperimentEntity> _experimentEntities;
+    private List<ExperimentEntity>? _experimentEntities = null;
     private int currentEntity = 0;
 
-    public DBDeckProvider()
+    private int? _lastId = null;
+
+    private readonly int _numberOfExperimentLimit;
+
+    public DBDeckProvider(int numberOfExperimentLimit = 100)
+    {
+        _numberOfExperimentLimit = numberOfExperimentLimit;
+    }
+
+    private void ReadEntities()
     {
         using (ApplicationContext appContext = new ApplicationContext())
         {
-            _experimentEntities = appContext.Experiments.ToList();
+            if (null == _lastId)
+            {
+                ReadEntitiesFirstTime(appContext);
+            }
+            else
+            {
+                ReadEntitiesSubsequent(appContext);
+            }
+
+            if (_experimentEntities.Count > 0)
+            {
+                _lastId = _experimentEntities[^1].Id;
+            }
+
+            currentEntity = 0;
         }
     }
 
-    // private void ReadEntities()
-    // {
-    //     using (ApplicationContext appContext = new ApplicationContext())
-    //     {
-    //         _experimentEntities = appContext.Experiments
-    //             .Where(e => e.Id / ReadAtOnce == currentPage)
-    //             .Take(ReadAtOnce)
-    //             .ToList();
-    //         ++currentPage;
-    //         currentEntity = 0;
-    //     }
-    // }
+    private void ReadEntitiesFirstTime(ApplicationContext appContext)
+    {
+        _experimentEntities = appContext.Experiments
+            .Take(ReadAtOnce)
+            .ToList();
+    }
+
+    private void ReadEntitiesSubsequent(ApplicationContext appContext)
+    {
+        _experimentEntities = appContext.Experiments
+            .Where(e => e.Id > _lastId)
+            .Take(ReadAtOnce)
+            .ToList();
+    }
 
     public Deck? GetDeck()
     {
-        ExperimentEntity? entity =
-            currentEntity < _experimentEntities.Count ? _experimentEntities[currentEntity++] : null;
-        if (null != entity)
+        if (null == _experimentEntities || currentEntity == _experimentEntities.Count)
         {
-            Console.WriteLine(entity.Id + ": " + entity.Deck.ToString(" "));
+            ReadEntities();
+            if (0 == _experimentEntities.Count)
+            {
+                return null;
+            }
         }
 
-        return entity?.Deck;
+        ExperimentEntity experimentEntity = _experimentEntities[currentEntity];
+
+        Console.WriteLine(experimentEntity.Id + ": " + experimentEntity.Deck.ToString(" "));
+
+        return _experimentEntities[currentEntity++].Deck;
     }
-    //{
-    // if (currentEntity == _experimentEntities.Count)
-    // {
-    //     ReadEntities();
-    //     if (0 == _experimentEntities.Count)
-    //     {
-    //         return null;
-    //     }
-    // }
-    //
-    // return _experimentEntities[currentEntity++].Deck;
-    //}
 }
